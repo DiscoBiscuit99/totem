142e1070d20124861ba810122cba21b6038da889
William Juhl <wijuh20@student.sdu.dk>
2022-07-15 21:16:56 +0200

    Added dt

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,6 +1,10 @@
 #include "engine.hpp"
 #include <unistd.h>
 
+#include <sys/time.h>
+#include <ctime>
+#include <chrono>
+
 void Engine::init() {
     state = State{};
 }
@@ -9,7 +13,7 @@ void Engine::hook_init( void (*fn)() ) {
     state.init_fn = fn;
 }
 
-void Engine::hook_update( void (*fn)( double dt ) ) {
+void Engine::hook_update( void (*fn)( time_t dt ) ) {
     state.update_fn = fn;
 }
 
@@ -20,23 +24,29 @@ void Engine::hook_render( void (*fn)() ) {
 
 void Engine::run() {
 
-    double dt = 0.001;
-
+    
     state.init_fn();
 
-    pid_t spawn = fork();
 
-    if ( spawn == 0 ) {
+    if ( fork() ) { // Might need to check for error -1
         while ( true ) {
             // BEGIN RENDER LOOP IN SEPARATE THREAD
             state.render_fn();
             // END RENDER LOOP
         }
     } else {
+        time_t dt = 0.0;
+        time_t start;
         while ( true ) {
+            start = time(nullptr) * 1000;
+            
             // BEGIN UPDATE LOOP IN SEPARATE THREAD
             state.update_fn( dt );
+
             // END UPDATE LOOP
+            dt = (time(nullptr) * 1000) - start;
+            
+            
        }
     }
 
--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <unistd.h>
 
 namespace Engine {
 
     struct State {
         void (*init_fn)() = nullptr;
-        void (*update_fn)( double dt ) = nullptr;
+        void (*update_fn)( time_t dt ) = nullptr;
         void (*render_fn)() = nullptr;
     } typedef State;
 
@@ -13,7 +14,7 @@ namespace Engine {
     void init();
 
     void hook_init( void (*fn)() );
-    void hook_update( void (*fn)( double dt ) );
+    void hook_update( void (*fn)( time_t dt ) );
     void hook_render( void (*fn)() );
 
     void run();
