c7de3487299f5df8ba5cb936f4812a643ea98744
William Juhl <wijuh20@student.sdu.dk>
2022-07-15 21:23:48 +0200

    Added code documentation

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,5 +1,5 @@
 #include "engine.hpp"
-#include <unistd.h>
+#include <unistd.h> // fork
 
 #include <sys/time.h>
 #include <ctime>
--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -3,6 +3,10 @@
 
 namespace Engine {
 
+    /**
+     * @brief This struct stores the pointers to the functions. MIght not be needed later...
+     * 
+     */
     struct State {
         void (*init_fn)() = nullptr;
         void (*update_fn)( time_t dt ) = nullptr;
@@ -11,12 +15,40 @@ namespace Engine {
 
     static State state;
 
+    /**
+     * @brief This function is called to initialize the state of the engine.
+     * 
+     */
     void init();
 
+    /**
+     * @brief This function hooks the given function to the state of the engine.
+     * This function is called once on engine_startup
+     * 
+     * @param fn The function pointer
+     */
     void hook_init( void (*fn)() );
+
+    /**
+     * @brief This function hooks the given function to the state of the engine.
+     * The delta-time is in milliseconds.
+     * The given function will be called as fast as the CPU allows.
+     * @param fn The function pointer
+     */
     void hook_update( void (*fn)( time_t dt ) );
+
+    /**
+     * @brief This function hooks the given function to the state of the engine.
+     * The given function will be called as fast as the CPU allows.
+     * Spawned on a different thread, so the execution-time of the updatefunction doesn't affect when this function is called.
+     * @param fn The function pointer
+     */
     void hook_render( void (*fn)() );
 
+    /**
+     * @brief This function starts the engine.
+     * 
+     */
     void run();
 
 }
